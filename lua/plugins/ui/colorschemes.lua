d6ef02b31661d886b3779c31794dd8475678d6b3
Joseph Quinn <81782398+josephjquinn@users.noreply.github.com>
2025-08-14 19:01:33 -0600

    colorschemes setup

---
+++ lua/plugins/ui/colorschemes.lua
@@ -0,0 +1,36 @@
+return {
+	{
+		"sainnhe/gruvbox-material",
+	},
+	{
+		"Mofiqul/dracula.nvim",
+
+		config = function()
+			vim.cmd([[colorscheme dracula-soft]])
+		end,
+	},
+	{
+		"folke/tokyonight.nvim",
+	},
+	{
+		"nyoom-engineering/oxocarbon.nvim",
+	},
+
+	{
+		"0xstepit/flow.nvim",
+
+	},
+
+	{
+		"sho-87/kanagawa-paper.nvim",
+
+	},
+  {
+    "rebelot/kanagawa.nvim",
+		-- config = function()
+		-- 	vim.cmd([[colorscheme kanagawa-dragon]])
+		-- end,
+
+  },
+}
+
