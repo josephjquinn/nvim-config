5851ae6ed250ff974817f8c3d46d0776bd2ee923
Joseph Quinn <81782398+josephjquinn@users.noreply.github.com>
2025-08-14 19:01:29 -0600

    statusline setup

---
+++ lua/plugins/ui/slimline.lua
@@ -0,0 +1,24 @@
+return {
+	"sschleemilch/slimline.nvim",
+	opts = {
+		style = "fg",
+		bold = true,
+		configs = {
+			path = {
+				hl = {
+					primary = "Label",
+				},
+			},
+			git = {
+				hl = {
+					primary = "Function",
+				},
+			},
+			filetype_lsp = {
+				hl = {
+					primary = "String",
+				},
+			},
+		},
+	},
+}
