1c0e0092fcbaecdcf3817db27673d2bf7a1e13a4
Joseph Quinn <81782398+josephjquinn@users.noreply.github.com>
2025-12-12 13:03:07 -0700

    updating keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -31,3 +31,28 @@ vim.keymap.set("n", "-", "<cmd>Lf<cr>", { desc = "Lf", silent = true })
 
 -- lazygit
 vim.keymap.set("n", "<leader>i", "<cmd>LazyGit<cr>", { desc = "Lazygit", silent = true })
+
+-- spell check toggle
+vim.keymap.set("n", "<leader>ls", "<cmd>set spell!<cr>", { desc = "Toggle spell check" })
+
+-- vimtex
+vim.keymap.set("n", "<leader>lc", "<cmd>VimtexCompile<cr>", { desc = "Compile Latex" })
+
+-- NeoTree
+-- keymap.set("n", "<leader>e", "<cmd>Neotree toggle position=left<cr>", { desc = "Toggle Explorer Left " })
+vim.keymap.set("n", "<leader>e", "<cmd>Neotree toggle reveal position=left<cr>", { desc = "Toggle Explorer Left " })
+vim.keymap.set("n", "<leader>o", function()
+	if vim.bo.filetype == "neo-tree" then
+		vim.cmd("wincmd p")
+	else
+		vim.cmd.Neotree("focus")
+	end
+end, { desc = "Toggle Explorer Focus" })
+vim.keymap.set("n", "-", function()
+	require("neo-tree.command").execute({
+		toggle = true,
+		reveal = true,
+		action = "focus",
+		position = "float",
+	})
+end, { desc = "Toggle Explorer Float" })
