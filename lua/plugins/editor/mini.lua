92f5ce4775253af53636c4be0737560502b7185c
Joseph Quinn <jquinn@Josephs-MacBook-Pro.local>
2025-12-15 19:55:44 -0700

    adding custom cli for mini picker repgrip

--- lua/plugins/editor/mini.lua
+++ lua/plugins/editor/mini.lua
@@ -1,115 +1,129 @@
 return {
-	{
-		"echasnovski/mini.extra",
-		config = function()
-			require("mini.extra").setup({})
-		end,
-	},
-	{
-		"echasnovski/mini.comment",
-		config = function()
-			require("mini.comment").setup({})
-		end,
-	},
-	{
-		"echasnovski/mini.indentscope",
-		config = function()
-			require("mini.indentscope").setup({})
-		end,
-	},
+  {
+    "echasnovski/mini.extra",
+    config = function()
+      require("mini.extra").setup({})
+    end,
+  },
 
-	{
-		"echasnovski/mini.jump2d",
-		config = function()
-			require("mini.jump2d").setup({
-				mappings = {
-					start_jumping = "gw",
-				},
+  {
+    "echasnovski/mini.comment",
+    config = function()
+      require("mini.comment").setup({})
+    end,
+  },
 
-				view = {
-					dim = true,
+  {
+    "echasnovski/mini.indentscope",
+    config = function()
+      require("mini.indentscope").setup({})
+    end,
+  },
 
-					n_steps_ahead = 0,
-				},
-			})
-		end,
-	},
+  {
+    "echasnovski/mini.jump2d",
+    config = function()
+      require("mini.jump2d").setup({
+        mappings = {
+          start_jumping = "gw",
+        },
+        view = {
+          dim = true,
+          n_steps_ahead = 0,
+        },
+      })
+    end,
+  },
 
-	{
-		"echasnovski/mini.pairs",
-		config = function()
-			require("mini.pairs").setup({})
-		end,
-	},
+  {
+    "echasnovski/mini.pairs",
+    config = function()
+      require("mini.pairs").setup({})
+    end,
+  },
 
-	{
-		"echasnovski/mini.surround",
-		config = function()
-			require("mini.surround").setup({})
-		end,
-	},
+  {
+    "echasnovski/mini.surround",
+    config = function()
+      require("mini.surround").setup({})
+    end,
+  },
 
-	{
-		"echasnovski/mini.clue",
-		config = function()
-			require("mini.clue").setup({
-				triggers = {
-					-- Leader key
-					{ mode = "n", keys = "<Leader>" },
-					{ mode = "x", keys = "<Leader>" },
+  {
+    "echasnovski/mini.clue",
+    config = function()
+      local clue = require("mini.clue")
 
-					-- Built-in completion
-					{ mode = "i", keys = "<C-x>" },
-					-- Marks
-					{ mode = "n", keys = "'" },
-					{ mode = "n", keys = "`" },
-					{ mode = "x", keys = "'" },
-					{ mode = "x", keys = "`" },
+      clue.setup({
+        triggers = {
+          { mode = "n", keys = "<Leader>" },
+          { mode = "x", keys = "<Leader>" },
 
-					-- Registers
-					{ mode = "n", keys = '"' },
-					{ mode = "x", keys = '"' },
-					{ mode = "i", keys = "<C-r>" },
-					{ mode = "c", keys = "<C-r>" },
+          { mode = "i", keys = "<C-x>" },
 
-					-- Window commands
-					{ mode = "n", keys = "<C-w>" },
+          { mode = "n", keys = "'" },
+          { mode = "n", keys = "`" },
+          { mode = "x", keys = "'" },
+          { mode = "x", keys = "`" },
 
-					-- keymaps key
-					{ mode = "n", keys = "f" },
-					{ mode = "n", keys = "s" },
-					{ mode = "n", keys = "g" },
-					{ mode = "n", keys = "c" },
-					{ mode = "n", keys = "l" },
-				},
-				clues = {
-					require("mini.clue").gen_clues.builtin_completion(),
-					require("mini.clue").gen_clues.g(),
-					require("mini.clue").gen_clues.marks(),
-					require("mini.clue").gen_clues.registers(),
-					require("mini.clue").gen_clues.windows(),
-					require("mini.clue").gen_clues.z(),
-				},
-				window = {
-					delay = 200,
-				},
-			})
-		end,
-	},
-	{
-"josephjquinn/mini.pick",
-		dependencies = {
-			"echasnovski/mini.icons",
-		},
-		config = function()
-			require("mini.icons").setup({})
-			require("mini.pick").setup({
-				window = {
-					width = 0.8,
-					height = 0.8,
-					border = "rounded",
-				},
-			})
-		end,
-	},
+          { mode = "n", keys = '"' },
+          { mode = "x", keys = '"' },
+          { mode = "i", keys = "<C-r>" },
+          { mode = "c", keys = "<C-r>" },
+
+          { mode = "n", keys = "<C-w>" },
+
+          { mode = "n", keys = "f" },
+          { mode = "n", keys = "s" },
+          { mode = "n", keys = "g" },
+          { mode = "n", keys = "c" },
+          { mode = "n", keys = "l" },
+        },
+
+        clues = {
+          clue.gen_clues.builtin_completion(),
+          clue.gen_clues.g(),
+          clue.gen_clues.marks(),
+          clue.gen_clues.registers(),
+          clue.gen_clues.windows(),
+          clue.gen_clues.z(),
+        },
+
+        window = {
+          delay = 200,
+        },
+      })
+    end,
+  },
+
+  {
+    "josephjquinn/mini.pick",
+    dependencies = {
+      "echasnovski/mini.icons",
+    },
+    config = function()
+      require("mini.icons").setup({})
+
+      local pick = require("mini.pick")
+
+      pick.setup({
+        window = {
+          width = 0.8,
+          height = 0.8,
+          border = "rounded",
+        },
+      })
+
+      -- Custom picker: includes hidden files (.gitignore, .env, etc.)
+      pick.registry.files_all = function()
+        pick.builtin.cli({
+          command = {
+            "rg",
+            "--files",
+            "--hidden",
+          },
+        })
+      end
+    end,
+  },
 }
