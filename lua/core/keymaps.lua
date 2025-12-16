98e56a29601c1b93fede119ef41490ff43d9c16b
Joseph Quinn <jquinn@Josephs-MacBook-Pro.local>
2025-12-16 16:09:42 -0700

    formatting

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -10,7 +10,7 @@ vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { desc = "Exit terminal mode", sile
 vim.keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
 vim.keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
 
--- mini.pick
+-- pickers (mini) 
 vim.keymap.set("n", "fb", "<cmd>Pick buffers<cr>", { desc = "Search open buffers" })
 vim.keymap.set("n", "ff", "<cmd>Pick files tool='rg'<cr>", { desc = "Search all files" })
 
@@ -21,7 +21,6 @@ vim.keymap.set("n", "fw", "<cmd>Pick grep_live tool='rg'<cr>", { desc = "Grep al
 vim.keymap.set("n", "fh", "<cmd>Pick help<cr>", { desc = "Search help tags" })
 vim.keymap.set("n", "fs", "<cmd>Pick spellsuggest<cr>", { desc = "Search spellcheck" })
 vim.keymap.set("n", "fc", "<cmd>Pick colorschemes<cr>", { desc = "Search colorschemds" })
-vim.keymap.set("n", "fd", "<cmd>Pick diagnostic<cr>", { desc = "Search diagnostics" })
 vim.keymap.set("n", "fh", "<cmd>Pick git_hunks<cr>", { desc = "Search git changes" })
 
 -- terminal
